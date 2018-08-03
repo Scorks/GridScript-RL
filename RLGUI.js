79675787a3059cc908bc3355e9a71b746eb917f5
Caro Strickland <carostrickland321@gmail.com>
2018-08-03 16:04:49 -0230

    added puck capabilities

--- RLGUI.js
+++ RLGUI.js
@@ -1,14 +1,14 @@
 RLGUI = function (container, map) {
-
+    
     var self = GUI(container);
-    self.env = Environment(map);
+    self.env = Environment(map);    
     self.pixelWidth = 640;
     self.pixelHeight = 640;
     self.sqSize = self.pixelWidth / self.env.width;
 
     // the colors used to draw the map
     self.colors = ["#777777", "#00ff00", "#0000ff"];
-
+    
     self.episodeTimer = 100;
     self.episode = [];
     self.lastEpisodeUpdate = performance.now();
@@ -16,13 +16,12 @@ RLGUI = function (container, map) {
 
     // draw the foreground, is called every 'frame'
     self.draw = function () {
-
-
+        
         // start the draw timer
         var t0 = performance.now();
         // clear the foreground to white
         self.fg_ctx.clearRect(0, 0, self.bg.width, self.bg.height);
-
+        
         // draw the tiles
         for (x = 0; x < self.env.width; x++) {
             for (y = 0; y < self.env.height; y++) {
@@ -37,18 +36,25 @@ RLGUI = function (container, map) {
         if (self.episode.length > 0) {
             let x = parseInt((self.episode[0][0][0] + 0.5) * self.sqSize);
             let y = parseInt((self.episode[0][0][1] + 0.5) * self.sqSize);
+            let px = parseInt((self.episode[0][0][2] + 0.5) * self.sqSize);
+            let py = parseInt((self.episode[0][0][3] + 0.5) * self.sqSize);
             self.drawCircle(x, y, self.sqSize/3, '#ff0000', '#ff0000')
+            self.drawCircle(px, py, self.sqSize/3, '#0000ff', '#0000ff')
         }
 
         // draw the policy
         self.fg_ctx.fillStyle = "#000000";
-        for (x = 0; x < self.env.width; x++) {
-            for (y = 0; y < self.env.height; y++) {
-
-                for (a = 0; a < self.env.actions.length; a++) {
-                    if (self.env.get(x,y) == 'X') { continue; }
-                    if (self.env.get(x,y) == 'T') { continue; }
-                    self.drawPolicyLine(x, y, self.env.actions[a], self.mc.P[x][y][a]);
+        if (self.episode.length > 0)
+        {
+            let px = self.episode[0][0][2];
+            let py = self.episode[0][0][3];
+            for (x = 0; x < self.env.width; x++) {
+                for (y = 0; y < self.env.height; y++) { 
+                    for (a = 0; a < self.env.actions.length; a++) {
+                        if (self.env.get(x,y) == 'X') { continue; }
+                        if (self.env.get(x,y) == 'T') { continue; }
+                        self.drawPolicyLine(x, y, self.env.actions[a], self.mc.P[x][y][px][py][a]);
+                    }
                 }
             }
         }
@@ -57,11 +63,11 @@ RLGUI = function (container, map) {
         self.fg_ctx.fillStyle = "#000000";
         for (y = 0; y <= self.env.height; y++) {
             self.fg_ctx.fillRect(0, y * self.sqSize, self.fg.width, 1);
-        }
-        for (x = 0; x <= self.env.width; x++) {
+        }   
+        for (x = 0; x <= self.env.width; x++) {  
             self.fg_ctx.fillRect(x * self.sqSize, 0, 1, self.fg.height);
         }
-
+        
         // do episode timer update
         let time = performance.now();
         if (self.episode.length > 0 && ((time - self.lastEpisodeUpdate) > self.episodeTimer)) {
@@ -97,7 +103,7 @@ RLGUI = function (container, map) {
     }
 
     self.doEpisodes = function() {
-
+        
         let config = self.getConfig();
         self.mc.config = config;
 
@@ -114,9 +120,9 @@ RLGUI = function (container, map) {
     self.getConfig = function() {
         return {
             stepSize        : parseFloat(document.getElementById('selectstep').value),
+            epsilon         : parseFloat(document.getElementById('selecteps').value),
             episodes        : parseInt(document.getElementById('selectepisodes').value),
-            maxEpisodeTime  : parseInt(document.getElementById('selecttime').value),
-            numPucks        : parseInt(document.getElementById('selectPucks').value)
+            maxEpisodeTime  : parseInt(document.getElementById('selecttime').value)
         };
     }
 
@@ -126,7 +132,7 @@ RLGUI = function (container, map) {
     }
 
     self.addEventListeners = function() {
-
+           
         self.fg.addEventListener('mousedown', function (evt) {
             var mousePos = self.getMousePos(self.fg, evt);
             var x = Math.floor(mousePos.x / self.sqSize);
@@ -135,13 +141,13 @@ RLGUI = function (container, map) {
                 if (self.mouse == 'print') {
                     console.log('Values[' + x + '][' + y + ']:\n', self.mc.Q[x][y]);
                     console.log('Policy[' + x + '][' + y + ']:\n', self.mc.P[x][y]);
-                }
+                } 
                 else if (self.mouse == 'wall') { self.env.set(x, y, 'X'); }
                 else if (self.mouse == 'clear') { self.env.set(x, y, -1); }
-                else if (self.mouse == 'terminal') { self.env.set(x, y, 'T'); }
+                else if (self.mouse == 'terminal') { self.env.set(x, y, 'T'); }            
             }
         }, false);
-
+    
         self.fg.oncontextmenu = function (e) {
             e.preventDefault();
         };
@@ -152,47 +158,45 @@ RLGUI = function (container, map) {
         self.bannerDiv  = self.create('div', 'BannerContainer',    self.fg.width + 30,   0, 400,  40);
         self.controlDiv = self.create('div', 'ControlContainer',   self.fg.width + 30,  60, 400, 600);
 
-        self.bannerDiv.innerHTML  = "<b>GridWorld Monte-Carlo ES RL with Pucks";
+        self.bannerDiv.innerHTML  = "<b>GridWorld Monte-Carlo ES RL - <a href='http://www.cs.mun.ca/~dchurchill/'>David Churchill</a>";
         self.controlDiv.innerHTML += "<label id='labelmouse'>Mouse Mode:</label>";
         self.controlDiv.innerHTML += "<label id='labelstep'>Step Size:</label>";
+        self.controlDiv.innerHTML += "<label id='labeleps'>Epsilon:</label>";
         self.controlDiv.innerHTML += "<label id='labeltime'>Max Episode Time:</label>";
         self.controlDiv.innerHTML += "<label id='labelepisodes'>Num Episodes:</label>";
-        self.controlDiv.innerHTML += "<label id='labelPucks'>Num Pucks:</label>"; // PUCK ADDITION
         self.controlDiv.innerHTML += "<select id='selectmouse' onchange='setMouse(value)';> \
                                         <option value='print'>Print Values</option> \
                                         <option value='wall'>Insert Wall</option> \
                                         <option value='clear'>Insert Clear</option> \
                                         <option value='terminal'>Insert Terminal</option></select>";
         self.controlDiv.innerHTML += "<input id='selectstep' type='number' min='0' max='1' step='0.1' value='0.1'>";
+        self.controlDiv.innerHTML += "<input id='selecteps' type='number' min='0' max='1' step='0.01' value='0.1'>";
         self.controlDiv.innerHTML += "<input id='selectepisodes' type='number' min='1' max='100000' step='1' value='1'>";
         self.controlDiv.innerHTML += "<input id='selecttime' type='number' min='1' max='1000' step='1' value='100'>";
-        self.controlDiv.innerHTML += "<input id='selectPucks' type='number' min='0' max='2' step='1' value='0'>";
-
+        
         self.controlDiv.innerHTML += "<button id='graphButton'>Perform Policy Iteration</button>";
-
+        
 
         var stylePrefix = 'position:absolute;';
         var ch = '25px', c1l = '0px', c2l = '150px', c3l = '425px', c1w = '140px', c2w = '250px', c3w = '150px';
-
+        
         document.getElementById('labelmouse').style     = stylePrefix + ' left:' + c1l + '; top:0;   width:' + c1w + '; height:' + ch + ';';
         document.getElementById('selectmouse').style    = stylePrefix + ' left:' + c2l + '; top:0;   width:' + c2w + '; height:' + ch + ';';
-
         document.getElementById('labelstep').style      = stylePrefix + ' left:' + c1l + '; top:40;   width:' + c1w + '; height:' + ch + '; ';
-        document.getElementById('selectstep').style    = stylePrefix + ' left:' + c2l + '; top:40;   width:' + c2w + '; height:' + ch + '; text-align: center;';
-
-        document.getElementById('labelepisodes').style  = stylePrefix + ' left:' + c1l + '; top:80;   width:' + c1w + '; height:' + ch + ';';
-        document.getElementById('selectepisodes').style = stylePrefix + ' left:' + c2l + '; top:80;   width:' + c2w + '; height:' + ch + '; text-align: center;';
-        document.getElementById('labeltime').style      = stylePrefix + ' left:' + c1l + '; top:120;   width:' + c1w + '; height:' + ch + ';';
-        document.getElementById('selecttime').style     = stylePrefix + ' left:' + c2l + '; top:120;   width:' + c2w + '; height:' + ch + '; text-align: center;';
-
-        document.getElementById('labelPucks').style      = stylePrefix + ' left:' + c1l + '; top:160;   width:' + c1w + '; height:' + ch + ';';
-        document.getElementById('selectPucks').style     = stylePrefix + ' left:' + c2l + '; top:160;   width:' + c2w + '; height:' + ch + '; text-align: center;';
+        document.getElementById('selectstep').style     = stylePrefix + ' left:' + c2l + '; top:40;   width:' + c2w + '; height:' + ch + '; text-align: center;';
+        document.getElementById('labeleps').style       = stylePrefix + ' left:' + c1l + '; top:80;   width:' + c1w + '; height:' + ch + '; ';
+        document.getElementById('selecteps').style      = stylePrefix + ' left:' + c2l + '; top:80;   width:' + c2w + '; height:' + ch + '; text-align: center;';
+        
+        document.getElementById('labelepisodes').style  = stylePrefix + ' left:' + c1l + '; top:120;   width:' + c1w + '; height:' + ch + ';';
+        document.getElementById('selectepisodes').style = stylePrefix + ' left:' + c2l + '; top:120;   width:' + c2w + '; height:' + ch + '; text-align: center;';
+        document.getElementById('labeltime').style      = stylePrefix + ' left:' + c1l + '; top:160;   width:' + c1w + '; height:' + ch + ';';
+        document.getElementById('selecttime').style     = stylePrefix + ' left:' + c2l + '; top:160;   width:' + c2w + '; height:' + ch + '; text-align: center;';
 
         document.getElementById('graphButton').style    = stylePrefix + ' left:0px; top:200;  width:200px' + '; height:' + ch + ';';
-
+        
         document.getElementById('graphButton').onclick  = function() { self.doEpisodes(); }
     }
-
+    
     self.setHTML();
     self.addEventListeners();
     self.mc = RLMC(self.env, self.getConfig());
--- RLMC_Student.js
+++ RLMC_Student.js
@@ -1,13 +1,12 @@
-// Caroline Strickland
-// 201215555
- RLMC = function(env, config) {
+RLMC = function(env, config) {
     var self = {};
     self.config = config;   // learning configuration settings
     self.env = env;         // the environment we will learn about
+    
     self.Q = [];            // values array Q[x][y][a] = value of doing action a at (x,y)
     self.P = [];            // policy array P[x][y][a] = probability of doing action a at (x,y)
-
-     self.init = function() {
+    
+    self.init = function() {
         // initialize all Q values to self.config.initialValue
         for (x=0; x<self.env.width; x++) {
             self.Q.push([]);
@@ -18,7 +17,8 @@
                 }
             }
         }
-         // initialize Policy to equiprobable actions
+
+        // initialize Policy to equiprobable actions
         for (x=0; x<self.env.width; x++) {
             self.P.push([]);
             for (y=0; y<self.env.height; y++) {
@@ -30,86 +30,31 @@
         }
     }
 
-     self.generateEpisode = function() {
-       console.log(self.config.numPucks);
+    self.generateEpisode = function() {
         // the episode should be in the following format:
         // episode[i] = [[state i x, state i y], action i, reward i]
         let episode = [];
 
-         // state is represented as [x, y]
+        // state is represented as [x, y]
         let state = [0, 0]; // initialize to a random non-blocked state in the environment
-        var isStateValid = true;
-        while (isStateValid) {
-          state = [Math.floor(Math.random()*(self.env.width-1)) + 1, Math.floor(Math.random()*(self.env.height-1)) + 1];
-          if (!self.env.isBlocked(state[0], state[1])) {
-            isStateValid = false;
-          }
-        }
-         // generate an episode with an upper bound on time
+
+        // generate an episode with an upper bound on time
         for (t=0; t<self.config.maxEpisodeTime; t++) {
             // episode generation pseudocode
             // references to functions not present should be implemented for convenience
-             // 1. If this is a terminal state, end the episode
-            if (self.env.isTerminal(state[0], state[1])) { // if we are currently in a terminal state
-              break; // end the episode
-            }
-            // first action chosen each episode should be random (exploring start)
-            if (t == 0) {
-              action = Math.floor(Math.random() * self.env.actions.length); // gets a random number between 0 and 3 (possible actions)
-            }
-            else {
-              var action = self.selectActionFromPolicy(state[0], state[1]); // selects the best action for the given state
-            }
-             // 3. reward = self.env.getReward(state[0], state[1], action)
-            var reward = self.env.getReward(state[0], state[1], action);
-
-             // 4. add [state, action, reward] to the episode
-            episodeVals = [[state[0], state[1]], action, reward];
-            // push current episode information into the episode array
-            episode.push(episodeVals);
-             // 5. state = getNextState(state[0], state[1], action)
-            state = self.getNextState(state[0], state[1], action);
+            // 1. If this is a terminal state, end the episode
+            // 2. action = selectActionFromPolicy(state[0], state[1]);
+            //    note: first action chosen each episode should be random (exploring starts)
+            // 3. reward = self.env.getReward(state[0], state[1], action)
+            // 4. add [state, action, reward] to the episode
+            // 5. state = getNextState(state[0], state[1], action)
         }
-         // return the episode
+
+        // return the episode
         return episode;
     }
 
- //---------------------------------------------------------------------------------------------------------
-
-     // selects best action from the current policy
-    self.selectActionFromPolicy = function(x, y) {
-      bestActionValue = 0;
-      bestAction = []; // the current best action to take based on values
-      for (var i = 0; i<self.env.actions.length; i++) {
-        if (self.P[x][y][i] > bestActionValue) {
-          bestActionValue = self.P[x][y][i];
-          bestAction = [];
-          bestAction.push(i);
-        }
-        else if (self.P[x][y][i] == bestActionValue) {
-          bestAction.push(i);
-        }
-      }
-      if (bestAction.length == 1) {
-        return bestAction[0]; // return the best action
-      }
-      else {
-        action = Math.floor(Math.random() * bestAction.length); // gets a random number between 0 and bestAction.length-1 (possible max actions)
-        return action;
-      }
-    }
-     // gets the next state given the current state and the chosen action
-    self.getNextState = function(x, y, action) {
-      next_x = x + self.env.actions[action][0];
-      next_y = y + self.env.actions[action][1];
-      if (self.env.isOOB(next_x, next_y) || self.env.isBlocked(next_x, next_y)) {
-        return [x, y];
-      }
-      else {
-        return [next_x, next_y];
-      }
-    }
-     // Student TODO: Implement this function
+    // Student TODO: Implement this function
     //
     // This funtion should update the self.Q values
     //
@@ -122,75 +67,15 @@
     //
     self.updateValues = function(episode) {
         // value update pseudocode
-
-        var sum = 0; // sum from t to the end of the episode
-        for (t=0; t<episode.length; t++) {
-          sum += episode[t][2];
-        }
-        // 2D array keeping track of visited states
-        visitedPairs = [];
-        for (x=0; x<self.env.width; x++) {
-            visitedPairs.push([]);
-            for (y=0; y<self.env.height; y++) {
-                visitedPairs[x].push([]);
-                for (a=0; a<self.env.actions.length; a++) {
-                    visitedPairs[x][y].push(false);
-                }
-            }
-        }
-
-        for (t=0; t<episode.length; t++) {
-          x = episode[t][0][0]; // x-coordinate of current position
-          y = episode[t][0][1]; // y-coordinate of current position
-          action = episode[t][1]; // action from the episode slice
-          currentReward = episode[t][2];
-
-          sum = sum - currentReward;
-          if (visitedPairs[x][y][action] == false) { // if this is our first time visiting this (state, action) pair
-            self.Q[x][y][action] = (self.Q[x][y][action] + (self.config.stepSize * (currentReward + sum - self.Q[x][y][action])));
-            visitedPairs[x][y][action] = true; // set visited to true so we don't update this Q value again during this episode
-          }
-        }
+        // for (t=0; t<episode.length; t++)
         //   R = sum rewards from t to the end of the episode
         //   if (first time s,a appears in the episode)
         //   update Q with new target of R using self.config.stepSize
-        return 1;
     }
 
-  // searches for an array (needle) within a 2D array (haystack)
-
-  self.searchForArray = function(haystack, needle) {
-    var i, j, current;
-    for(i = 0; i < haystack.length; ++i){
-      if(needle.length === haystack[i].length){
-        current = haystack[i];
-        for(j = 0; j < needle.length && needle[j] === current[j]; ++j);
-        if(j === needle.length)
-          return i;
-        }
-      }
-      return -1;
-    }
-
-  // sums all items within an array
-  self.sum = function(input) {
-
-    if (toString.call(input) !== "[object Array]")
-    return false;
-
-    var total =  0;
-    for(var i=0;i<input.length;i++) {
-      if(isNaN(input[i])){
-        continue;
-      }
-        total += Number(input[i]);
-      }
-      return total;
-    }
-
-     // Student TODO: Implement this function
+    // Student TODO: Implement this function
     //
-    // This function should update the policy to reflect the current values.
+    // This funtion should update the policy to reflect the current values.
     // It should update the policy for
     //
     // Args:
@@ -200,48 +85,16 @@
     //    none
     //
     self.updatePolicy = function(episode) {
+        // policy update pseudocode
+        // for (each state (x,y) in the episode)
+        //   maxActionValue = get maximum action value from Q[x][y]
+        //   maxActions = [which actions gave maxActionValue]
+        //   
+        //   for each action a
+        //     if (a in maxActions) P[x][y][a] = 1.0/maxActions.length
+        //     else                 P[x][y][a] = 0
+    }
 
-      // 2D array keeping track of visited states
-      visitedPairs = [];
-      for (x=0; x<self.env.width; x++) {
-          visitedPairs.push([]);
-          for (y=0; y<self.env.height; y++) {
-              visitedPairs[x].push([]);
-              for (a=0; a<self.env.actions.length; a++) {
-                  visitedPairs[x][y].push(false);
-              }
-          }
-      }
-
-      for (t=0; t<episode.length; t++) {
-        x = episode[t][0][0]; // x-coordinate of current position
-        y = episode[t][0][1]; // y-coordinate of current position
-        action = episode[t][1]; // action from the episode slice
-
-        if (visitedPairs[x][y][action] == false) { // if the state has not been visited before
-          // maxActionValue = get maximum action value from Q[x][y]
-          maxVal = -1000000;
-          maxIndices = []
-          for (a=0; a<self.env.actions.length; a++){
-            if (self.Q[x][y][a] > maxVal) {
-              maxVal = self.Q[x][y][a];
-              maxIndices = [];
-              maxIndices.push(a);
-            }
-            else if (self.Q[x][y][a] == maxVal) { // if it is equal
-              maxIndices.push(a);
-            }
-            }
-          }
-          for (a=0; a<self.env.actions.length; a++) {
-            self.P[x][y][a] = 0;
-            }
-          for (a=0; a<maxIndices.length; a++) {
-            self.P[x][y][maxIndices[a]] = 1.0 / maxIndices.length
-          }
-          visitedPairs[x][y][action] = true;
-          }
-        }
-     self.init();
+    self.init();
     return self;
 }
